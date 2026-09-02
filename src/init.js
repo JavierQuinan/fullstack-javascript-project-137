98ddb7a139c9fcb5b21048b36a5855a3c18aa300
Francisco Quinteros <155692279+JavierQuinan@users.noreply.github.com>
2026-09-02 08:46:49 -0500

    fix: consume corrected RSS description field

--- src/init.js
+++ src/init.js
@@ -9,11 +9,10 @@ import i18next from 'i18next';
 import locale from './locales/yupLocale.js';
 import resources from './locales/index.js';
 import parse from './rss.js';
-// Watchers separated
 import watch from './watchers.js';
 
 const fetchingTimeout = 5000;
-// Adding proxy in a separate function
+
 const addProxy = (url) => {
   const urlWithProxy = new URL('/get', 'https://allorigins.hexlet.app');
   urlWithProxy.searchParams.set('url', url);
@@ -34,7 +33,6 @@ const getLoadingProcessErrorType = (e) => {
 const fetchNewPosts = (watchedState) => {
   const promises = watchedState.feeds.map((feed) => {
     const urlWithProxy = addProxy(feed.url);
-    // axios is not modified, URL is generated above
     return axios
       .get(urlWithProxy)
       .then((response) => {
@@ -46,9 +44,6 @@ const fetchNewPosts = (watchedState) => {
         const oldPosts = watchedState.posts.filter(
           (post) => post.channelId === feed.id,
         );
-        // We are finding differences, and the function catches this idea
-        // There are numerous ways to evaluate the uniqueness
-        // Here we pick a title
         const posts = differenceWith(
           newPosts,
           oldPosts,
@@ -57,18 +52,16 @@ const fetchNewPosts = (watchedState) => {
         watchedState.posts.unshift(...posts);
       })
       .catch((e) => {
-        // The failure to fetch one post must not disrupt fetching others
         console.error(e);
       });
   });
+
   Promise.all(promises).finally(() => {
-    // Run a new iteration when the previous one is over
     setTimeout(() => fetchNewPosts(watchedState), fetchingTimeout);
   });
 };
 
 const loadRss = (watchedState, url) => {
-  // Working with state one a single abstraction layer
   watchedState.loadingProcess.status = 'loading';
   const urlWithProxy = addProxy(url);
   return axios
@@ -79,7 +72,7 @@ const loadRss = (watchedState, url) => {
         url,
         id: uniqueId(),
         title: data.title,
-        description: data.descrpition,
+        description: data.description,
       };
       const posts = data.items.map((item) => ({
         ...item,
@@ -105,7 +98,6 @@ const loadRss = (watchedState, url) => {
 };
 
 export default () => {
-  // Query elements inside function, not on a module level
   const elements = {
     form: document.querySelector('.rss-form'),
     input: document.querySelector('.rss-form input'),
@@ -117,10 +109,7 @@ export default () => {
   };
 
   if (elements.input) elements.input.setAttribute('aria-label', 'url');
-  // Two processess, two states
-  // Posts are normalized
-  // A state must always have a meaningful name, no empty string or null
-  // The app state is defined within a function, not on a module level
+
   const initState = {
     feeds: [],
     posts: [],
@@ -141,9 +130,6 @@ export default () => {
     },
   };
 
-  // i18next creates a global state when initialized. On the one hand,
-  // this is handy as you can import an i18next object into any module to use there.
-  // On the other hand, the tests are starting to affect each other
   const i18nextInstance = i18next.createInstance();
 
   const promise = i18nextInstance
@@ -153,26 +139,23 @@ export default () => {
       resources,
     })
     .then(() => {
-      // Texts are not stored in state
       yup.setLocale(locale);
       const baseUrlSchema = yup.string().url().required();
 
       const validateUrl = (url, feeds) => {
         const feedUrls = feeds.map((feed) => feed.url);
         const actualUrlSchema = baseUrlSchema.notOneOf(feedUrls);
-        // async functions only
         return actualUrlSchema
           .validate(url)
           .then(() => null)
           .catch((e) => e.message);
       };
-      // The state is passed as a function parameter along with the i18next instance
+
       const watchedState = watch(elements, initState, i18nextInstance);
 
       // @ts-ignore
       elements.form.addEventListener('submit', (e) => {
         e.preventDefault();
-        // Getting form data via FormData
         // @ts-ignore
         const data = new FormData(e.target);
         const url = data.get('url');
@@ -208,7 +191,6 @@ export default () => {
         watchedState.ui.seenPosts.add(id);
       });
 
-      // Use setTimeout (not a setInterval) since the network is unreliable; wait for the completion
       setTimeout(() => fetchNewPosts(watchedState), fetchingTimeout);
     });
 
